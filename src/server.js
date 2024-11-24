1748998ddbe8beaab1574ac1a847a83cff79d88c
Daria Daleka <sovyshkamy@gmail.com>
2024-11-24 17:55:59 +0100

    add homework

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import cors from "cors";
 import pino from "pino";
-import { initMongoConnection } from "./db/initMongoConnection.js";
 import { PORT } from "./utils/env.js";
-import contactsRoutes from "./routes/contactsRoutes.js";
+import contactsRoute from "./routers/contacts.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import  notFoundHandler  from "./middlewares/notFoundHandler.js";
 
 const logger = pino({
   level: "info",
@@ -13,19 +14,15 @@ const logger = pino({
 });
 
 export const setupServer = async () => {
-  await initMongoConnection();
-
   const app = express();
   app.use(cors());
   app.use(express.json());
 
-  app.use("/contacts", contactsRoutes);
+  app.use("/contacts", contactsRoute);
 
-  app.use((req, res, next) => {
-    res.status(404).json({
-      message: "Not found",
-    });
-  });
+  app.use(notFoundHandler);
+
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     logger.info(`Server is running on port ${PORT}`);
