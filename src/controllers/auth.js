f701ec3072de4d2bf89580fa589c096994e811c4
Daria Daleka <sovyshkamy@gmail.com>
2024-12-15 14:23:03 +0100

    add reset password

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,7 @@
 
-import { loginUser, registerUser, logoutUser, refreshUsersSession} from '../services/auth.js';
+import { loginUser, registerUser, logoutUser, refreshUsersSession, requestResetToken, resetPassword} from '../services/auth.js';
 import { ONE_DAY } from '../constants/constants.js';
+import {  } from '../services/auth.js';
 
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
@@ -69,5 +70,23 @@ export const refreshUserSessionController = async (req, res) => {
     data: {
       accessToken: session.accessToken,
     },
+  });
+};
+
+export const requestResetEmailController = async (req, res) => {
+  await requestResetToken(req.body.email);
+  res.json({
+    message: 'Reset password email was successfully sent!',
+    status: 200,
+    data: {},
+  });
+};
+
+export const resetPasswordController = async (req, res) => {
+  await resetPassword(req.body);
+  res.json({
+    message: 'Password was successfully reset!',
+    status: 200,
+    data: {},
   });
 };
